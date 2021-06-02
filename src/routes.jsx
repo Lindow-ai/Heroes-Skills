e4b320948b72bfc12e7890595481b783c5b70d5b
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2021-06-02 17:02:23 +0200

    Add route + page + entry point + Login page

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import Login  from './pages/Login'
+import Dashboard  from './pages/Dashboard'
+
+const routes = [
+    {
+        'name': 'Login',
+        'path': '/',
+        'component': Login
+    },
+    {
+        'name': 'Dashboard',
+        'path': '/Dashboard',
+        'component': Dashboard
+    }
+]
+
+export default routes
