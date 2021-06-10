39bbe8bba7f5cad9be47b534a3fe46121ca7b0ea
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2021-06-10 10:06:58 +0200

    Add Stepper

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -18,12 +18,32 @@ import { ReactVideo } from "reactjs-media"
 import { Collapse } from '@material-ui/core';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
+import Stepper from '@material-ui/core/Stepper';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Step from '@material-ui/core/Step';
+
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+  },
+  root2: {
+    width: '100%'
+  },
+  button: {
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2)
+  },
+  resetContainer: {
+    padding: theme.spacing(3)
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
@@ -44,10 +64,47 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const getSteps = () => {
+  return ['Presentation', 'How to hack time ?', 'Third', 'Fourth', 'Onhovered item', 'Number five'];
+}
+
+const getStepContent = (step) => {
+  switch(step) {
+    case 0:
+      return <Typography paragraph>test 
+sdsqd
+      </Typography>;
+    case 1:
+      return 'An ad group contains one or more ads which target a shared set of keywords.';
+    case 2:
+      return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+    default:
+      return 'Unknown step';
+  }
+}
+
+
 export default function ClippedDrawer() {
   const [open, setOpen] = useState(true);
-
+  const [activeStep, setActiveStep] = React.useState(0);
   const classes = useStyles();
+  const steps = getSteps();
+
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
 
   const handleClick = () => {
     setOpen(!open);
@@ -79,7 +136,46 @@ export default function ClippedDrawer() {
             </ListItem>
             <Collapse in={open} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-                <ListItem button className={classes.nested} >
+                <div className={classes.root2}>
+                <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((label, index) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+            <StepContent>
+              <Typography>{getStepContent(index)}</Typography>
+              <div className={classes.actionsContainer}>
+                <div>
+                  <Button
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    className={classes.button}
+                  >
+                    Back
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                </div>
+              </div>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} className={classes.resetContainer}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} className={classes.button}>
+            Reset
+          </Button>
+        </Paper>
+      )}
+                </div>
+                {/* <ListItem button className={classes.nested} >
                   <ListItemText primary="1) Presentation" />
                 </ListItem>
                 <ListItem button className={classes.nested} >
@@ -96,7 +192,7 @@ export default function ClippedDrawer() {
                 </ListItem>
                 <ListItem button className={classes.nested} >
                   <ListItemText primary="6) Number Five" />
-                </ListItem>
+                </ListItem> */}
               </List>
             </Collapse>
           </List>
